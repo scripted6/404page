6b5abfc4774a83976a96d3f512fc1ff1e4e46756
Nico <71660522+scripted6@users.noreply.github.com>
2022-11-30 17:51:31 -0500

    replace base

--- daily-ui-008-404-page.markdown
+++
@@ -1,7 +0,0 @@
-Daily UI #008 - 404 Page
-------------------------
-
-
-A [Pen](https://codepen.io/totallynico/pen/XWYPOqB) by [Nico](https://codepen.io/totallynico) on [CodePen](https://codepen.io).
-
-[License](https://codepen.io/license/pen/XWYPOqB).
--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-        // Parallax Code
-        var scene = document.getElementById('scene');
-        var parallax = new Parallax(scene);
+anime({
+  targets: '.row svg',
+  translateY: 10,
+  autoplay: true,
+  loop: true,
+  easing: 'easeInOutSine',
+  direction: 'alternate'
+});
+
+anime({
+  targets: '#zero',
+  translateX: 10,
+  autoplay: true,
+  loop: true,
+  easing: 'easeInOutSine',
+  direction: 'alternate',
+  scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+    rotateY: {value: '+=180', delay: 200},
+});
